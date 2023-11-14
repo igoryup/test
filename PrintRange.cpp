206d60433138c14fe9ce9b5669a76325f9a64ed5
Igor Yurakov <yurakov99@gmail.com>
2023-11-15 00:24:03 +0300

    print

--- PrintRange.cpp
+++ PrintRange.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <numeric>
 #include <vector>
 #include <algorithm>
 #include <cmath>
@@ -10,14 +12,15 @@
 #include <set>
 #include <unordered_set>
 #include <queue>
+#include <random>
 
 using namespace std;
 
 template <typename IteratorType>
-void PrintRange(IteratorType it1, IteratorType it2){
+void PrintRange(IteratorType it1, IteratorType it2) {
     bool first = true;
-    for (auto it = it1; it != it2; ++it){
-        if (!first){
+    for (auto it = it1; it != it2; ++it) {
+        if (!first) {
             std::cout << ' ';
         }
         first = false;
@@ -27,19 +30,107 @@ void PrintRange(IteratorType it1, IteratorType it2){
 }
 
 template <typename Container, typename ContainerElement>
-void FindAndPrint(const Container& container, const ContainerElement& element){
+void FindAndPrint(const Container& container, const ContainerElement& element) {
     auto new_end_it = find(container.begin(), container.end(), element);
     PrintRange(container.begin(), new_end_it);
     PrintRange(new_end_it, container.end());
 }
 
 template <typename IteratorType>
-auto MakeVector(IteratorType it1, IteratorType it2){
+auto MakeVector(IteratorType it1, IteratorType it2) {
     return std::vector(it1, it2);
 }
 
+// функция, записывающая элементы диапазона в строку
+template <typename It>
+string PrintRangeToString(It range_begin, It range_end) {
+    // удобный тип ostringstream -> https://ru.cppreference.com/w/cpp/io/basic_ostringstream
+    ostringstream out;
+    for (auto it = range_begin; it != range_end; ++it) {
+        out << *it << " "s;
+    }
+    out << endl;
+    // получаем доступ к строке с помощью метода str для ostringstream
+    return out.str();
+}
+
+// функция сортировки слиянием
+template <typename It>
+void SwapVal(It it1, It it2) {
+    auto buf = *it1;
+    *it1 = *it2;
+    *it2 = buf;
+}
+template <typename It>
+void MergeSortedRanges(It first_begin, It first_end, It second_begin, It second_end) {
+    // Итератор для первого поддиапазона
+    It i = first_begin;
+    
+    // Итератор для второго поддиапазона
+    It j = second_begin;
+
+    while (i < first_end && j < second_end) {
+        if (*i < *j) {
+            ++i;
+        } else {
+            auto buf = *j;
+            for (auto k = j; k != i; --k) {
+                *(k) = *(k - 1);
+            }
+            *(i) = buf;
+            ++i;
+            ++j;
+            ++first_end;
+        }
+    }
+}
+
+// функция сортировки слиянием
+template <typename It>
+void MergeSort(It range_begin, It range_end) {
+    int size = range_end - range_begin;
+    if (size == 2){
+         if (*(range_begin + 1) < *(range_begin)) {
+            SwapVal(range_begin, (range_begin + 1));
+        }
+    }
+    if (size > 2) {
+        It range_mid = range_begin + size / 2;
+        MergeSort(range_begin, range_mid);
+        MergeSort(range_mid, range_end);
+        MergeSortedRanges(range_begin, range_mid, range_mid, range_end);
+    }
+}
+
+
+template <typename It>
+vector<string> GetPermutations(It container_begin, It container_end) {
+    sort(container_begin, container_end);
+    reverse(container_begin, container_end);
+    vector<string> result;
+    do {
+        result.push_back(PrintRangeToString(container_begin, container_end));
+    } while (prev_permutation(container_begin, container_end));
+    return result;
+}
+
 int main() {
-    set<string> unique_langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s};
-    vector<string> langs = MakeVector(unique_langs.begin(), unique_langs.end());
-    PrintRange(langs.begin(), langs.end()); 
+    vector<int> test_vector(11);
+    // iota             -> http://ru.cppreference.com/w/cpp/algorithm/iota
+    // Заполняет диапазон последовательно возрастающими значениями
+    iota(test_vector.begin(), test_vector.end(), 1);
+
+    // shuffle   -> https://ru.cppreference.com/w/cpp/algorithm/random_shuffle
+    // Перемешивает элементы в случайном порядке
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(test_vector.begin(), test_vector.end(), g);
+
+    // Выводим вектор до сортировки
+    PrintRange(test_vector.begin(), test_vector.end());
+    // Сортируем вектор с помощью сортировки слиянием
+    MergeSort(test_vector.begin(), test_vector.end());
+    // Выводим результат
+    PrintRange(test_vector.begin(), test_vector.end());
+    return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,46 +1,337 @@
-#include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
 #include <cmath>
-#include <deque>
-#include <list>
+#include <iostream>
 #include <map>
-#include <unordered_map>
+#include <numeric>
 #include <set>
-#include <unordered_set>
-#include <queue>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
 
-int main()
-{
-    std::priority_queue<int> pq;
-    int num;
-    std::string command;
-    while (std::cin >> command)
-    {
-        if (command == "CLEAR")
-        {
-            while (!pq.empty())
-            {
-                pq.pop();
+using namespace std;
+
+const int MAX_RESULT_DOCUMENT_COUNT = 5;
+
+string ReadLine() {
+    string s;
+    getline(cin, s);
+    return s;
+}
+
+int ReadLineWithNumber() {
+    int result;
+    cin >> result;
+    ReadLine();
+    return result;
+}
+
+vector<string> SplitIntoWords(const string& text) {
+    vector<string> words;
+    string word;
+    for (const char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
             }
         }
-        else if (command == "ADD")
-        {
-            std::cin >> num;
-            pq.push(num);
+        else {
+            word += c;
         }
-        else if (command == "EXTRACT")
-        {
-            if (pq.empty())
-            {
-                std::cout << "CANNOT\n";
-            }
-            else
-            {
-                std::cout << pq.top() << "\n";
-                pq.pop();
+    }
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+
+    return words;
+}
+
+struct Document {
+    Document() = default;
+
+    Document(int id, double relevance, int rating)
+        : id(id)
+        , relevance(relevance)
+        , rating(rating) {
+    }
+
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
+
+template <typename StringContainer>
+set<string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    set<string> non_empty_strings;
+    for (const string& str : strings) {
+        if (!str.empty()) {
+            non_empty_strings.insert(str);
+        }
+    }
+    return non_empty_strings;
+}
+
+enum class DocumentStatus {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
+
+class SearchServer {
+public:
+
+    template <typename StringContainer>
+    explicit SearchServer(const StringContainer& stop_words)
+        : stop_words_(MakeUniqueNonEmptyStrings(stop_words)) {
+        for (const auto& word : stop_words_) {
+            if (!IsValidWord(word)) {
+                throw invalid_argument("В стоп-словах есть спец.символы");
             }
         }
     }
+
+    explicit SearchServer(const string& stop_words_text)
+        : SearchServer(
+            SplitIntoWords(stop_words_text)) {
+    }
+
+    void AddDocument(int document_id, const string& document, DocumentStatus status,
+        const vector<int>& ratings) { // теперь SplitIntoWordsNoStop() сам пробрасывает исключения, и тут нужны только связанные с ID 
+
+        if (document_id < 0) {
+            throw invalid_argument("Отрицательный ID");
+        }
+        if (documents_.count(document_id) != 0) {
+            throw invalid_argument("Уже существующий ID");
+        }
+
+        const auto words = SplitIntoWordsNoStop(document);
+        const double inv_word_count = 1.0 / words.size();
+        for (const string& word : words) {
+            word_to_document_freqs_[word][document_id] += inv_word_count;
+        }
+        documents_.emplace(document_id, DocumentData{ ComputeAverageRating(ratings), status });
+        // новый id контейнер
+        documents_id.push_back(document_id);
+    }
+
+    template <typename DocumentPredicate>
+    vector<Document> FindTopDocuments(const string& raw_query, DocumentPredicate document_predicate) const { // теперь ParseQuery() сам пробрасывает исключения, поэтому тут они не нужны
+
+        auto query = ParseQuery(raw_query);
+        auto matched_documents = FindAllDocuments(query, document_predicate);
+
+        sort(matched_documents.begin(), matched_documents.end(),
+            [](const Document& lhs, const Document& rhs) {
+                if (abs(lhs.relevance - rhs.relevance) < std::numeric_limits<double>::epsilon()) {
+                    return lhs.rating > rhs.rating;
+                }
+                return lhs.relevance > rhs.relevance;
+            });
+        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
+            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+        }
+        return matched_documents;
+
+
+    }
+
+    vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status) const {
+        return FindTopDocuments(
+            raw_query, [status](int document_id, DocumentStatus document_status, int rating) {
+                return document_status == status;
+            });
+    }
+
+    vector<Document> FindTopDocuments(const string& raw_query) const {
+        return FindTopDocuments(raw_query, DocumentStatus::ACTUAL);
+    }
+
+    int GetDocumentCount() const {
+        return documents_.size();
+    }
+
+    int GetDocumentId(int index) const {
+        if (index >= documents_.size()) {
+            throw out_of_range("Слишком большой индекс документа");//
+        }
+        if (index < 0) {
+            throw out_of_range("Отрицательный индекс документа");//
+        }
+        return documents_id.at(index); // .at() сам пробросит нужное исключение (как и сказано собвственно в подсказке к задаче, спасибо что напомнили)
+    }
+
+    tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query, int document_id) const {
+
+        auto query = ParseQuery(raw_query);
+        vector<string> matched_words;
+        for (const string& word : query.plus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            if (word_to_document_freqs_.at(word).count(document_id)) {
+                matched_words.push_back(word);
+            }
+        }
+        for (const string& word : query.minus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            if (word_to_document_freqs_.at(word).count(document_id)) {
+                matched_words.clear();
+                break;
+            }
+        }
+        return { matched_words, documents_.at(document_id).status };
+    }
+
+    static bool IsValidWord(const string& word) {
+        return none_of(word.begin(), word.end(), [](char c) {
+            return c >= '\0' && c < ' ';
+            });
+    }
+
+private:
+    struct DocumentData {
+        int rating;
+        DocumentStatus status;
+    };
+    const set<string> stop_words_;
+    map<string, map<int, double>> word_to_document_freqs_;
+    map<int, DocumentData> documents_;
+    vector<int> documents_id;
+
+    bool IsStopWord(const string& word) const {
+        return stop_words_.count(word) > 0;
+    }
+
+    vector<string> SplitIntoWordsNoStop(const string& text) const {
+        vector<string> words;
+        for (const string& word : SplitIntoWords(text)) {
+
+            if (!IsValidWord(word)) {
+                throw invalid_argument("В словах документа есть спец.символы");
+            }
+            if (!IsStopWord(word)) {
+                words.push_back(word);
+            }
+        }
+        return words;
+    }
+
+    static int ComputeAverageRating(const vector<int>& ratings) {
+        if (ratings.empty()) {
+            return 0;
+        }
+        // каждый раз когда копирую код из тренажера, accumulate пропадает, а я и не проверяю(
+        int rating_sum = accumulate(ratings.begin(), ratings.end(), 0);
+        return rating_sum / static_cast<int>(ratings.size());
+    }
+
+    struct QueryWord {
+        string data;
+        bool is_minus;
+        bool is_stop;
+    };
+
+    QueryWord ParseQueryWord(string text) const {
+        bool is_minus = false;
+        // Word shouldn't be empty
+        if (text[0] == '-') {
+            is_minus = true;
+            text = text.substr(1);
+        }
+        return { text, is_minus, IsStopWord(text) };
+    }
+
+    struct Query {
+        set<string> plus_words;
+        set<string> minus_words;
+    };
+
+    Query ParseQuery(const string& text) const {
+        Query query;
+        for (const string& word : SplitIntoWords(text)) {
+            const QueryWord query_word = ParseQueryWord(word);
+            if (query_word.data[0] == '-') {
+                throw invalid_argument("В словах поискового запроса есть слова с двумя минусами '--'");
+            }
+            if (!IsValidWord(query_word.data)) {
+                throw invalid_argument("В словах поискового запроса есть спец.символы");
+            }
+            if (query_word.data.empty()) {
+                throw invalid_argument("В словах поискового запроса есть пустые минус слова");
+            }
+            if (!query_word.is_stop) {
+                if (query_word.is_minus) {
+                    query.minus_words.insert(query_word.data);
+                }
+                else {
+                    query.plus_words.insert(query_word.data);
+                }
+            }
+        }
+        return query;
+    }
+
+    // Existence required
+    double ComputeWordInverseDocumentFreq(const string& word) const {
+        return log(GetDocumentCount() * 1.0 / word_to_document_freqs_.at(word).size());
+    }
+
+    template <typename DocumentPredicate>
+    vector<Document> FindAllDocuments(const Query& query,
+        DocumentPredicate document_predicate) const {
+        map<int, double> document_to_relevance;
+        for (const string& word : query.plus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            const double inverse_document_freq = ComputeWordInverseDocumentFreq(word);
+            for (const auto [document_id, term_freq] : word_to_document_freqs_.at(word)) {
+                const auto& document_data = documents_.at(document_id);
+                if (document_predicate(document_id, document_data.status, document_data.rating)) {
+                    document_to_relevance[document_id] += term_freq * inverse_document_freq;
+                }
+            }
+        }
+
+        for (const string& word : query.minus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            for (const auto [document_id, _] : word_to_document_freqs_.at(word)) {
+                document_to_relevance.erase(document_id);
+            }
+        }
+
+        vector<Document> matched_documents;
+        for (const auto [document_id, relevance] : document_to_relevance) {
+            matched_documents.push_back(
+                { document_id, relevance, documents_.at(document_id).rating });
+        }
+        return matched_documents;
+    }
+
+
+};
+
+// ==================== для примера =========================
+void PrintDocument(const Document& document) {
+    cout << "{ "s
+        << "document_id = "s << document.id << ", "s
+        << "relevance = "s << document.relevance << ", "s
+        << "rating = "s << document.rating << " }"s << endl;
+}
+int main() {
+    try {
+        SearchServer search_server("и в на \x12"s);
+
+    }
+    catch (...) {
+        cout << "поймали ошибку";
+    }
+
 }
